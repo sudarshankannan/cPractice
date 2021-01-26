a981d30dccb6166afa5ca9c4a6f0a22ae561c037
Sudarshan Kannan <sudarshan.kannan99@gmail.com>
2021-01-25 18:26:01 -0800

    hello

--- bitFuncs.c
+++ bitFuncs.c
@@ -5,16 +5,27 @@ int insertion(int num1, int num2, int i, int j){
     // Output: N=10001001100
      
 }
-void printBitsInReverse(int num, int bitPlace){
+//prints an individual bit
+void printBitAt(int num, int bitPlace){
+
+}
+//printBitsInReverse
+void printAllBits(int num){
     int count = 0;
     int temp;
     int valAtBit;
-    while(count <= bitPlace){
-        temp = num>>1;
-        printf("%d", temp & 1);
+    int output;
+    if(num==0){
+        printf("%d", 0);
+    }    
+    while(num>0){
+        temp = num;
+        output = temp & 1;
+        printf("%d", output);
+        num = num<<1;
     }
 }
-
+//main method
 int main(){
-
+    printAllBits(13);
 }
